f2c16360b93f5705e9cc8e7b21749961586e4f95
wolflou1 <118213768+Louie-Wolf@users.noreply.github.com>
2023-12-07 18:51:18 +0100

    complete lab12

--- lab12/code_ASM/app/main.s
+++ lab12/code_ASM/app/main.s
@@ -45,20 +45,25 @@ main            PROC
 
                 ; Configure NVIC (enable interrupt channel)
                 ; STUDENTS: To be programmed
-
-
+				LDR 	R0,=REG_SETENA0
+				LDR 	R1,[R0]
                 ; END: To be programmed 
 
                 ; Initialize variables
                 ; STUDENTS: To be programmed	
-
+				LDR 	R2, =0x10000040
+				ORRS 	R1,R2			;make sure I dont overwrite the others
+				STR 	R1,[R0]
 
                 ; END: To be programmed
 
 loop
                 ; Output counter on 7-seg
                 ; STUDENTS: To be programmed
-
+				LDR  	R0, =REG_CT_7SEG
+                LDR 	R1, =counter_per_interval
+                LDR 	R1, [R1]
+                STRH 	R1, [R0]
 
                 ; END: To be programmed
 
@@ -70,8 +75,19 @@ loop
 ; Handler for EXTI0 interrupt
 ; -----------------------------------------------------------------------------
                  ; STUDENTS: To be programmed
-
-
+EXTI0_IRQHandler	PROC
+					EXPORT EXTI0_IRQHandler
+					PUSH 	{LR}
+					
+					LDR		R0, =counter
+					LDR		R1,[R0]
+					ADDS	R1,#1
+					STR		R1,[R0]
+					
+					BL 		clear_IRQ_EXTI0
+					
+					POP		{PC}
+					ENDP
                  ; END: To be programmed
 
  
@@ -79,8 +95,26 @@ loop
 ; Handler for TIM2 interrupt
 ; -----------------------------------------------------------------------------
                 ; STUDENTS: To be programmed
-
-
+TIM2_IRQHandler		PROC
+					EXPORT TIM2_IRQHandler
+					PUSH 	{LR}
+					LDR 	R0,=LED_16_31
+					LDR		R1,[R0]
+					LDR		R2,=0x0000FFFF
+					EORS	R1,R2
+					STRH	R1,[R0]
+					
+					LDR		R0,=counter
+					LDR		R1,[R0]
+					LDR		R2,=counter_per_interval
+					STR		R1,[R2]
+					MOVS	R1,#0
+					STR		R1,[R0]
+					
+					BL 		clear_IRQ_TIM2
+					
+					POP		{PC}
+					ENDP
                 ; END: To be programmed
                 ALIGN
 
@@ -91,7 +125,8 @@ loop
                 AREA myVars, DATA, READWRITE
 
                 ; STUDENTS: To be programmed
-
+counter					DCD 0
+counter_per_interval	DCD 0
 
                 ; END: To be programmed
 
